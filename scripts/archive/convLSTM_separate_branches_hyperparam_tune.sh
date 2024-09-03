90007f4851485795949d5a073bbadc77b24dc7a8
Varun Khandekar <varun.khandekar23@imperial.ac.uk>
2024-09-03 16:30:44 +0100

    prepping online repo

---
+++ scripts/archive/convLSTM_separate_branches_hyperparam_tune.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#SBATCH --gres=gpu:teslaa40:1
+#SBATCH --mail-type=ALL # required to send email notifcations
+#SBATCH --mail-user=vk223
+#SBATCH --output=convLSTM_sep_branch_hyperparam_tune_%j.out
+
+export PROJECT_FLOOD_DATA="/homes/vk223/ProjectFlood/static/imperial_data_paths.json"
+export PROJECT_FLOOD_CORE_PATHS="/homes/vk223/ProjectFlood/static/imperial_core_paths.json"
+export PROJECT_FLOOD_REPO_DIR="/homes/vk223/ProjectFlood"
+source /vol/bitbucket/vk223/project_flood/projectfloodvenv/bin/activate
+source /vol/cuda/11.8.0/setup.sh
+export PYTHONPATH=$PYTHONPATH:${PROJECT_FLOOD_REPO_DIR}/
+export PROJECT_FLOOD_SCRIPTPATH=model_runs/convLSTM_separate_branches_hyperparam_tune.py
+export PROJECT_FLOOD_SCRIPTLOC=${PROJECT_FLOOD_REPO_DIR}/${PROJECT_FLOOD_SCRIPTPATH}
+TERM=vt100 
+#TERM=xterm
+
+export NINPUT=2500
+
+echo -e "\n\n\n\n\n\n\n\n"
+echo -e "\n******************************************************************************"
+echo -e "\n**********************************STARTING************************************"
+echo -e "\n******************************************************************************"
+/usr/bin/nvidia-smi
+python3 -V
+
+echo -e "\n\n\n"
+python3 $PROJECT_FLOOD_SCRIPTLOC -n $NINPUT
+
+
+echo -e "\n\n\n\n\n\n\n\n"
+echo -e "\n******************************************************************************"
+echo -e "\n**********************************COMPLETE************************************"
+echo -e "\n******************************************************************************"
+/usr/bin/nvidia-smi
+uptime
