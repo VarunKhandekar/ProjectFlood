a838a8d7be48cc8b661a7de8bc15a6d883870e44
Varun Khandekar <varun.khandekar23@imperial.ac.uk>
2024-09-18 09:52:32 +0100

    VAE work: POST PROJECT

--- model_runs/model_run_helpers_cvae.py
+++ model_runs/model_run_helpers_cvae.py
@@ -11,7 +11,7 @@ from visualisations.visualisation_helpers import *
 # Loss function: Reconstruction loss + KL Divergence
 def KLReconstruction_loss_function(recon_x, x, mu, logvar, beta=1.0):
     # Reconstruction loss (binary cross-entropy)
-    BCE = F.binary_cross_entropy(recon_x, x, reduction='sum')
+    BCE = torch.nn.BCELoss(recon_x, x, reduction='sum')
     
     # KL Divergence: D_KL(Q(z|x) || P(z))
     KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
